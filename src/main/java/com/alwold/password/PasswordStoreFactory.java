ac219a2a1d412bc34882dd2230c53fab07cf12db
Al Wold <alwold@gmail.com>
2010-11-08 16:30:52 -0700

    add method to factory to specify store file

--- src/main/java/com/alwold/password/PasswordStoreFactory.java
+++ src/main/java/com/alwold/password/PasswordStoreFactory.java
@@ -9,4 +9,13 @@ public class PasswordStoreFactory {
 		return new PasswordSafePasswordStore();
 	}
 
+	public static PasswordStore getPasswordStore(String storeLocation) throws PasswordStoreException {
+		if (storeLocation.endsWith("psafe3")) {
+			return new PasswordSafePasswordStore(storeLocation);
+		} else if (storeLocation.endsWith("keychain")) {
+			return new OsXKeychainPasswordStore(storeLocation);
+		} else {
+			throw new PasswordStoreException("Cannot determine store type from filename");
+		}
+	}
 }
