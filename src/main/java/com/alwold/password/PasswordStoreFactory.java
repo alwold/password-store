75af6b9901f72adc253219900b4337b804afde59
Al Wold <alwold@gmail.com>
2010-11-16 17:28:57 -0700

    use one instance per store location

--- src/main/java/com/alwold/password/PasswordStoreFactory.java
+++ src/main/java/com/alwold/password/PasswordStoreFactory.java
@@ -1,21 +1,33 @@
 package com.alwold.password;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author alwold
  */
 public class PasswordStoreFactory {
-	public static PasswordStore getPasswordStore() {
-		return new PasswordSafePasswordStore();
+	private static Map<String, PasswordStore> instances = new HashMap<String, PasswordStore>();
+	private static PasswordStore instance;
+	
+	public static synchronized PasswordStore getPasswordStore() {
+		if (instance == null) {
+			instance = new PasswordSafePasswordStore();
+		}
+		return instance;
 	}
 
-	public static PasswordStore getPasswordStore(String storeLocation) throws PasswordStoreException {
-		if (storeLocation.endsWith("psafe3")) {
-			return new PasswordSafePasswordStore(storeLocation);
-		} else if (storeLocation.endsWith("keychain")) {
-			return new OsXKeychainPasswordStore(storeLocation);
-		} else {
-			throw new PasswordStoreException("Cannot determine store type from filename");
+	public static synchronized PasswordStore getPasswordStore(String storeLocation) throws PasswordStoreException {
+		if (instances.get(storeLocation) == null) {
+			if (storeLocation.endsWith("psafe3")) {
+				instances.put(storeLocation, new PasswordSafePasswordStore(storeLocation));
+			} else if (storeLocation.endsWith("keychain")) {
+				instances.put(storeLocation, new OsXKeychainPasswordStore(storeLocation));
+			} else {
+				throw new PasswordStoreException("Cannot determine store type from filename");
+			}
 		}
+		return instances.get(storeLocation);
 	}
 }
