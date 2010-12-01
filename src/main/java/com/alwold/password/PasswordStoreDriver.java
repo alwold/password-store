8b22e7d5b64c503eec0b97faa70de0b4f2084906
Al Wold <alwold@gmail.com>
2010-12-01 15:57:16 -0700

    - register driver on load - support pwstore url format

--- src/main/java/com/alwold/password/PasswordStoreDriver.java
+++ src/main/java/com/alwold/password/PasswordStoreDriver.java
@@ -2,39 +2,59 @@ package com.alwold.password;
 
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class PasswordStoreDriver implements Driver {
-	private Driver driver;
-	
-	public PasswordStoreDriver() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
-		driver = (Driver) Class.forName("oracle.jdbc.OracleDriver").newInstance();
+	static {
+		try {
+			DriverManager.registerDriver(new PasswordStoreDriver());
+		} catch (SQLException ex) {
+			ex.printStackTrace(System.out);
+		}
+		// TODO initialize drivers: oracle, sybase, mysql, mssql, jtds
 	}
-
+	
 	public boolean acceptsURL(String url) throws SQLException {
-		return driver.acceptsURL(url);
+		if (url.startsWith("jdbc:pwstore:")) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public Connection connect(String url, Properties info) throws SQLException {
-		return driver.connect(url, info);
+		url = url.replace(":pwstore:", ":");
+		try {
+			return ((Driver) Class.forName(info.getProperty("realDriver")).newInstance()).connect(url, info);
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+						throw new RuntimeException(e);
+
+		}
 	}
 
 	public int getMajorVersion() {
-		return driver.getMajorVersion();
+		return 1;
 	}
 
 	public int getMinorVersion() {
-		return driver.getMinorVersion();
+		return 0;
 	}
 
 	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
-		return driver.getPropertyInfo(url, info);
+		return new DriverPropertyInfo[]{
+			new DriverPropertyInfo("user", info.getProperty("user")), 
+			new DriverPropertyInfo("password", info.getProperty("password"))};
 	}
 
 	public boolean jdbcCompliant() {
-		return driver.jdbcCompliant();
+		return false;
 	}
 	
 }
